913e701fc913f6906743b0a20f5b56c6e0ceb726
cradiator <cradiator@gmail.com>
2014-03-04 23:56:39 +0800

    Add NFA status and NFA class.

---
+++ Regex/AutomataStatus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex
+{
+    class AutomataStatus
+    {
+        public AutomataStatus()
+        {
+            s_iCurrentStatusNumber++;
+            m_iStatusNumber = s_iCurrentStatusNumber;
+            m_mapInputTargetStatus = new Dictionary<char, List<AutomataStatus>>();
+        }
+
+        public void AddTransition(char cInputAscii, AutomataStatus targetStatus)
+        {
+            // Get the target status list for cInputAscii.
+            List<AutomataStatus> list;
+            if (m_mapInputTargetStatus.ContainsKey(cInputAscii))
+            {
+                list = m_mapInputTargetStatus[cInputAscii];
+            }
+            else
+            {
+                list = new List<AutomataStatus>();
+                m_mapInputTargetStatus.Add(cInputAscii, list);
+            }
+
+            list.Add(targetStatus);
+        }
+
+        // Dictionary<input, target_status>
+        Dictionary<char, List<AutomataStatus>> m_mapInputTargetStatus;
+        private int m_iStatusNumber;
+        private static int s_iCurrentStatusNumber = 0;
+    }
+}
---
+++ Regex/NFA.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex
+{
+    class NFA
+    {
+
+        public static NFA CreateFromInput(char input)
+        {
+            AutomataStatus start = new AutomataStatus();
+            AutomataStatus end = new AutomataStatus();
+            start.AddTransition(input, end);
+         
+            NFA nfa = new NFA();
+            nfa.startStatus_ = start;
+            nfa.endStatus_ = end;
+
+            return nfa;
+        }
+
+        public static NFA ConcatNFA(NFA op1, NFA op2)
+        {
+            AutomataStatus start = new AutomataStatus();
+            AutomataStatus end = new AutomataStatus();
+            start.AddTransition((char)0, op1.startStatus_);
+            op2.endStatus_.AddTransition((char)0, end);
+            op1.endStatus_.AddTransition((char)0, op2.startStatus_);
+
+            NFA nfa = new NFA();
+            nfa.startStatus_ = start;
+            nfa.endStatus_ = end;
+            return nfa;
+        }
+
+        /*
+        public static NFA ConcatNFA(NFA nfa1, NFA nfa2)
+        {
+
+        }
+         */
+
+        private NFA()
+        {
+            startStatus_ = null;
+            endStatus_ = null;
+        }
+        private AutomataStatus startStatus_;
+        private AutomataStatus endStatus_;
+    }
+}
